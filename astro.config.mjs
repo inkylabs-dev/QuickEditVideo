5268dee0ab45e459dbee75185a5a7f241dcf8216
soasme <soasme@gmail.com>
2025-08-16 22:40:18 +1200

    Add sitemap integration and update robots.txt; remove dynamic sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://quickeditvideo.com',
     integrations: [tailwind(), preact({
         compat: true
     }), sitemap()],
