d36964e454f65d03819da3497bf7669e708f8b0e
soasme <soasme@gmail.com>
2025-12-19 21:52:46 +1300

    feat: add 'force-static' dynamic export to multiple page components for improved static rendering

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-static';
+
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import type { LayoutProps } from '../../src/components/Layout';
